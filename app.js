835acb465d690040422a19d506e1c124b8a20f1a
MilosIvankovicEon <miloscane@gmail.com>
2021-04-02 22:44:20 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+//Server
+var server				=	require('express')();
+var http				=	require('http').Server(server);
+var net					=	require('net');
+var io					=	require('socket.io')(http);
+var express				=	require('express');
+var fs					=	require('fs');
+var bodyParser			=	require('body-parser');
+var session				=	require('express-session');
+var cookieParser		=	require('cookie-parser');
+var crypto				=	require('crypto');
+
+server.set('view engine','ejs');
+var viewArray	=	[__dirname+'/views'];
+var viewFolder	=	fs.readdirSync('views');
+for(var i=0;i<viewFolder.length;i++){
+	if(viewFolder[i].split(".").length==1){
+		viewArray.push(__dirname+'/'+viewFolder[i])
+	}
+}
+server.set('views', viewArray);
+server.use(express.static(__dirname + '/public'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(cookieParser());
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('Server Started');
+});
+
+var mainFileVersion	=	1.1;
+
+var pageInfo	=	{};
+
+pageInfo.fileVersion	=	mainFileVersion;
+pageInfo.siteName		=	"Lupo - Handmade shoes";
+
+require('events').EventEmitter.prototype._maxListeners = 0;
+
+//Minify resourcessss
+/*var htmlModulesList	=	fs.readdirSync('./public/html-modules');
+for(var i=0;i<htmlModulesList.length;i++){
+	if(htmlModulesList[i].split('.').length>1){
+		var code	=	fs.readFileSync('./public/html-modules/'+htmlModulesList[i],'utf-8');
+		code	=	code.replace(/[\t\n\r]/gm,'');
+		fs.writeFileSync('./public/html-modules/minified/'+htmlModulesList[i].split('.')[0]+'.min.'+htmlModulesList[i].split('.')[1],code);
+	}
+}*/
+
+function fetchPageInfo(pageName){
+	var pageInfoObject	=	JSON.parse(JSON.stringify(pageInfo));
+	if(fs.existsSync('./info/'+ pageName + ".json")){
+		var pageJson	=	JSON.parse(fs.readFileSync('./info/'+ pageName + ".json",'utf-8'));
+	}else{
+		var pageJson	=	JSON.parse(fs.readFileSync('./info/home.json','utf-8'));
+	}
+	for(var i=0;i<Object.keys(pageJson).length;i++){
+		pageInfoObject[Object.keys(pageJson)[i]]	=	pageJson[Object.keys(pageJson)[i]].toString();
+	}
+
+	return pageInfoObject
+}
+
+/*io.on('connection', function(socket){
+	
+	socket.on('disconnect',function(){
+		
+	});
+});*/
+
+
+server.get('/',function(req,res){
+	res.render('home',{
+		pageInfo: fetchPageInfo('home')
+	});
+});
+
+server.get('*',function(req,res){
+	res.redirect('/');
+});
---
+++ public/js/map.js
@@ -0,0 +1,183 @@
+var map;
+var myLatLng;
+var marker;
+
+function initMap() {
+	myLatLng = {lat: 44.856760, lng: 20.361069};//51.896468, 4.419172
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: myLatLng,
+		zoom: 14,
+		styles: [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f0e1d0"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f0e1d0"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#a4c1a7"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ad9891"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f9c891"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#aed9ff"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+]
+	});
+	marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		title: 'Lupo'
+	});
+	
+	/*var contentString = '<div style="text-align:center;"><div style="color:rgba(37,78,118,1); font-weight:bold; margin-top:10px; font-size:14px;">Mobatec<br>Headquarters</div></div>';
+
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString
+	});
+	infowindow.open(map,marker);*/
+}
